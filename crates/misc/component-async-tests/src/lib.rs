a0dd052c873eb4bd6c097e5bf58308e28d6348b2
Victor Adossi <vadossi@cosmonic.com>
2025-02-01 04:18:09 +0900

    test: add multiple send as initial context stream test
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/misc/component-async-tests/src/lib.rs
+++ crates/misc/component-async-tests/src/lib.rs
@@ -1913,4 +1913,36 @@ mod test {
             &fs::read(test_programs_artifacts::ASYNC_ERROR_CONTEXT_CALLEE_COMPONENT).await?;
         test_run(&compose(caller, callee).await?).await
     }
+
+    #[tokio::test]
+    async fn async_error_context_roundtrip() -> Result<()> {
+        let caller =
+            &fs::read(test_programs_artifacts::ASYNC_ERROR_CONTEXT_CALLER_COMPONENT).await?;
+        let callee =
+            &fs::read(test_programs_artifacts::ASYNC_ERROR_CONTEXT_CALLEE_COMPONENT).await?;
+        test_run(&compose(caller, callee).await?).await
+    }
+
+    // No-op function; we only test this by composing it in `async_error_context_stream_callee`
+    #[allow(
+        dead_code,
+        reason = "here only to make the `assert_test_exists` macro happy"
+    )]
+    fn async_error_context_stream_callee() {}
+
+    // No-op function; we only test this by composing it in `async_error_context_stream_caller`
+    #[allow(
+        dead_code,
+        reason = "here only to make the `assert_test_exists` macro happy"
+    )]
+    fn async_error_context_stream_caller() {}
+
+    #[tokio::test]
+    async fn async_stream_end_err() -> Result<()> {
+        let caller =
+            &fs::read(test_programs_artifacts::ASYNC_ERROR_CONTEXT_STREAM_CALLER_COMPONENT).await?;
+        let callee =
+            &fs::read(test_programs_artifacts::ASYNC_ERROR_CONTEXT_STREAM_CALLEE_COMPONENT).await?;
+        test_run(&compose(caller, callee).await?).await
+    }
 }
